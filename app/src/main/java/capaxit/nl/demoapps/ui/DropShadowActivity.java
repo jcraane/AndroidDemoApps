df4b3460834794709173ffc6e7cbf7965bfcc242
jcraane <jcraane@gmail.com>
2016-10-25 16:19:19 +0200

    Dropshadow button.

---
+++ app/src/main/java/capaxit/nl/demoapps/ui/DropShadowActivity.java
@@ -0,0 +1,15 @@
+package capaxit.nl.demoapps.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import capaxit.nl.demoapps.R;
+
+public class DropShadowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drop_shadow);
+    }
+}
